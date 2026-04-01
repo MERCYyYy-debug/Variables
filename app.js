1fe26b954d451a93239f887079dd1c3e9305f4cf
Mercy <mbgraphicx@gmail.com>
2026-04-01 14:45:08 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+let firstNum = 1;
+let secondNum = 2;
+const sum = firstNum + secondNum;
+console.log(sum);
+
+// this is a string
+var myName = "John";
+console.log(myName);
+
+
+// ths is a boolean
+let isStudent = 3;
+console.log(isStudent);
+
+
+const myArray = [1, 2, 3, 4, 5];
+const namesArray = ["Alice", "Dean", "Esther"]
+console.log(myArray)
+console.log(namesArray)
+
+const person = {
+    name: "Alice",
+    age: 33,
+    isStudent: true
+}
+console.log(person)
